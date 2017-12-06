01e9d5777587d1134688dedc4ee85585329575e9
Jon Drew <jon.drew1492@gmail.com>
2017-12-06 23:53:12 +0000

    Create sold out badge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def conditional_wrapper
+    product_quantity = 0 #product.quantity
+    if product_quantity == 0
+      "Sold Out"
+    else
+      product_quantity
+    end
+  end
 end
