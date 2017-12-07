0073622693d809697ab4856aff46ce9da5181c8a
Jon Drew <jon.drew1492@gmail.com>
2017-12-07 15:55:10 +0000

    Edit db seed to make dress shirt quantity 0

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
-  def conditional_wrapper
-    product_quantity = 0 #product.quantity
-    if product_quantity == 0
+  def conditional_wrapper(product)
+    if product.quantity == 0
       "Sold Out"
     else
-      product_quantity
+      product.quantity
     end
   end
 end
